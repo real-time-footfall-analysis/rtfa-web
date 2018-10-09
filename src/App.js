747b5399d450e90b102222d926087115dbf2ea4b
Zeshan Amjad <zeshan@zesh.me>
2018-10-09 09:02:17 +0100

    Add change to test S3 deployment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Realtime Footfall Analysis
+            Realtime Footfall Analysis | 3rd Year Project
           </p>
           <a
             className="App-link"
