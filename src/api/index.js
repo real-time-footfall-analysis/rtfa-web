6afd8867b60437f5c277bd59f330205e9b085907
Zeshan Amjad <zeshan@zesh.me>
2018-10-29 16:51:02 +0000

    Refactor API calls to reduce duplication

--- src/api/index.js
+++ src/api/index.js
@@ -8,20 +8,46 @@ const eventsURL = `${BASE_URL}/events`;
 export default {
   events: {
     getAll: async organiserID => {
-      const eventsResponse = axios.get(eventsURL, {
-        params: {
-          organiserId: organiserID
-        }
+      const eventData = await request.get(eventsURL, {
+        organiserId: organiserID
       });
-      eventsResponse.catch(console.error);
-      const eventData = (await eventsResponse).data;
       /* Convert array into object keyed by eventID. */
       return _.keyBy(eventData, "eventID");
     },
     create: async newEvent => {
-      let newEventResponse = axios.post(eventsURL, newEvent);
-      newEventResponse.catch(console.error);
-      return (await newEventResponse).data;
+      return request.post(eventsURL, newEvent);
     }
   }
+};
+
+const request = {
+  get: async (url, params) => {
+    return executeRequest("GET", url, {
+      params: {
+        ...params
+      }
+    });
+  },
+  post: async (url, payload) => {
+    return executeRequest("POST", url, payload);
+  }
+};
+
+const executeRequest = async (type, url, payload) => {
+  let response;
+  switch (type) {
+    case "GET":
+      response = axios.get(url, payload);
+      break;
+    case "POST":
+      response = axios.post(url, payload);
+      break;
+    default:
+      response = {};
+      console.error(
+        `You are trying to execute the following invalid type of request: ${type}`
+      );
+  }
+  response.catch(console.error);
+  return (await response).data;
 };
