40cce8d1481bf79c02373b8edaf68bcd1297ed8e
Zeshan Amjad <zeshan@zesh.me>
2018-11-28 15:44:44 +0000

    Add internal state handlers to SendNotificationForm

--- src/components/Pages/NotificationsPage/SendNotificationForm/SendNotificationForm.js
+++ src/components/Pages/NotificationsPage/SendNotificationForm/SendNotificationForm.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import TextField from "../../../UI/TextField/TextField";
 import { MultiSelect } from "@blueprintjs/select";
 import Button from "../../../UI/Button/Button";
+import { MenuItem, Tag } from "@blueprintjs/core";
 
-const SendNotificationForm = () => {
-  return (
-    <section>
-      <h2>Send New Notification</h2>
-      <TextField label="Title" />
-      <TextField label="Description" />
-      <MultiSelect
-        tagRenderer={tag => <span>{tag}</span>}
-        items={["Region 1", "Region 2", "Region 3"]}
-        itemRenderer={item => item}
-        onItemSelect={console.log}
-      />
-      <br />
-      <Button>Submit</Button>
-    </section>
-  );
-};
+class SendNotificationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      description: "",
+      selectedRegions: []
+    };
+  }
+  render() {
+    return (
+      <section>
+        <h2>Send New Notification</h2>
+        <TextField
+          label="Title"
+          value={this.state.title}
+          onChange={event => this.setState({ title: event.target.value })}
+        />
+        <TextField
+          label="Description"
+          value={this.state.description}
+          onChange={event => this.setState({ description: event.target.value })}
+        />
+        <MultiSelect
+          tagRenderer={tag => <Tag>{tag}</Tag>}
+          items={["Campsite Bar", "Arena Bar", "Main Stage"]}
+          itemRenderer={(item, { modifiers }) => (
+            <MenuItem active={modifiers.active} key={item} text={item} />
+          )}
+          onItemSelect={console.log}
+        />
+        <br />
+        <Button>Submit</Button>
+      </section>
+    );
+  }
+}
 
 SendNotificationForm.propTypes = {};
 
